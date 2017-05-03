54c65a9523db08c241661555f351775510c9c9dc
Maciej Drahusz <maciejdrahusz@gmail.com>
2017-05-03 22:34:43 +0200

    OK finished

--- zad2/data.js
+++ zad2/data.js
@@ -1,5 +1,14 @@
  var products = {
-    "product1": "P1 Showing",
-    "product2": "P2 Showing",
-    "product3": "P3 Showing",
+    "p1": {
+    	"class" : "p1",
+    	"msg": "product 1 is showing"
+	},
+    "p2": {
+    	"class" : "p2",
+    	"msg": "product 2 is showing"
+	},
+	"p3": {
+    	"class" : "p3",
+    	"msg": "product 3 is showing"
+	}
  }
--- zad2/script.js
+++ zad2/script.js
@@ -1,8 +1,30 @@
-// console.log(products.product1)
+function countChecked () {
+	var score = 0, selection = [];
+	
+	$('#form :checked').each(function() {
+	   selection += $(this).val();
+	   score += parseInt($(this).val());
+	});
+  	if(score < 3){
+  		setProduct('p1');
+  	}
+ 	if(score < 5 && score >= 3){
+  		setProduct('p2');
+  	}
+  	if(score >= 5 && selection === '24'){
+  		setProduct('p1');
+  	}
+  	if(score >= 5 && selection !== '24'){
+  		setProduct('p3');
+  	}
 
-var countChecked = function() {
-  var n = $( "input:checked" );
-  $( ".output" ).text( n + (n === 1 ? " is" : " are") + " checked!" );
 };
+
+function setProduct(product){
+	$('.output').text(eval('products.' + product + '.msg'));
+	$(".product > header").removeClass();
+	$(".product > header").addClass(product);
+}
+
 countChecked();
-	$( "input[type=checkbox]" ).on( "click", countChecked );
+$( "input[type=checkbox]" ).on( "click", countChecked );
